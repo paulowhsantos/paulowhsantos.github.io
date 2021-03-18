73b7071ff72ee33a1b7db30feb0e0fd47b69f9b3
Paulo Henrique <42096755+paulowh@users.noreply.github.com>
2021-03-18 14:59:35 -0300

    certificados iniciais

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let imagens = document.querySelector('.lbox');
+let modal = document.querySelector('.boxImg');
+let modalImg = document.querySelector('#boxImg');
+let btClose = document.querySelector('#btn_close');
