5f4173014949d106922a284f9553ccbf7270692f
Paulo - Trabalho <ph744915@gmail.com>
2021-03-19 12:27:47 -0300

    Experiencia
    Complemento das experiencia

--- js/script.js
+++
@@ -1,4 +0,0 @@
-let imagens = document.querySelector('.lbox');
-let modal = document.querySelector('.boxImg');
-let modalImg = document.querySelector('#boxImg');
-let btClose = document.querySelector('#btn_close');
